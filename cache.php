ff18fdd3b29ea39ccd741ca6b233b01080da4ccb
aligate <aligate26@gmail.com>
2017-03-21 00:02:10 +0100

    Update cache.php

--- cache.php
+++ cache.php
@@ -1,7 +1,8 @@
 <?php
 require_once 'conf.php';
 
-class Cache{
+class Cache
+{
 	
 private $path;	
 private $data;
@@ -16,24 +17,22 @@ public function __construct(Conf $conf, $data)
 
 public function setCache($key, $seconds=3600)
 {
-	
-	$file = $this->path->getPath($this).'/'.$key.'.txt';
-	$content['data'] = $this->data;
+	$file = $this -> path-> getPath ($this).'/'.$key.'.txt';
+	$content['data'] = $this -> data;
 	$content['end_time'] = time() + $seconds;
-	file_put_contents($file, serialize($content));
+	file_put_contents ( $file, serialize($content ));
 }
 
 public function getCache($key)
 {
-	$file = $this->path->getPath($this).'/'.$key.'.txt';
+	$file = $this -> path -> getPath ($this).'/'.$key.'.txt';
 	if(file_exists($file))
 	{
-		
 		$content = unserialize(file_get_contents($file));
 		
-		if ( time()< $content['end_time']) 
+		if ( time() < $content ['end_time'] ) 
 		{
-			return $content['data'];
+			return $content ['data'];
 		} 
 		else {
 			unlink($file);
