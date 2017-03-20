c459195ea74bdc52a44435ac221a4165cd6ce10b
aligate <aligate26@gmail.com>
2017-03-20 21:02:16 +0100

    Update logger.php

--- logger.php
+++ logger.php
@@ -4,9 +4,8 @@ require_once 'conf.php';
 
 class Logger{
 	
-public $path;
+private $path;
 public $product;
-public $method;
 	
 	public function __construct(Conf $conf, Product $product){
 	
@@ -16,13 +15,12 @@ public $method;
 
 }
 
- function __call($method_name, $args) {
-       $this->method =  date('m-d-Y H:i').' Method: '.$method_name;
+ function __call($method_name, $args) 
+ {
+       $method =  date('m-d-Y H:i').' Method: '.$method_name;
 	   $file = $this->path->getPath($this).'/'.time().'.txt';
-	   file_put_contents($file, $this->method);
+	   file_put_contents($file, $method);
  }
-
- 
 }
 
 
