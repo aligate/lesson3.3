f9a04e6395b2749030c3231f6318912e0f7d98a7
aligate <aligate26@gmail.com>
2017-03-26 23:02:21 +0200

    Update cache.php

--- cache.php
+++ cache.php
@@ -15,17 +15,17 @@ public function __construct(Conf $conf, $data)
 
 }
 
-public function setCache($key, $seconds=3600)
+public function setCache($file_key, $seconds=3600)
 {
-	$file = $this -> path-> getPath ($this).'/'.$key.'.txt';
+	$file = $this -> path-> getPath ($this).'/'.$file_key.'.txt';
 	$content['data'] = $this -> data;
 	$content['end_time'] = time() + $seconds;
 	file_put_contents ( $file, serialize($content ));
 }
 
-public function getCache($key)
+public function getCache($file_key)
 {
-	$file = $this -> path -> getPath ($this).'/'.$key.'.txt';
+	$file = $this -> path -> getPath ($this).'/'.$file_key.'.txt';
 	if(file_exists($file))
 	{
 		$content = unserialize(file_get_contents($file));
