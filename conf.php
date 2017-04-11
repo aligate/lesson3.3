6c324197b6995d574a7b297ecba18c0e0c895280
aligate <aligate26@gmail.com>
2017-04-11 13:57:53 +0200

    Update conf.php

--- conf.php
+++ conf.php
@@ -4,16 +4,18 @@ class Conf{
 
 private $pathArr = ['Cache' => 'tmp/cache', 'Logger' => 'tmp/logs'];
 
+private $pathTo;
+
 public function getPath($obj){
+	
+	$this->pathTo = $obj;
 	
 	foreach($this->pathArr as $key =>$path){
 		
-		if($obj instanceof $key) return $path;
+		if(get_class($this->pathTo) == $key) return $path;
 	}
 }
 								
-				
-
 }
 
 
